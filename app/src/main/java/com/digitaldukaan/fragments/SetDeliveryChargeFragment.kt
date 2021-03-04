600361666aba69895460016b99f2bc0a2dae1d8a
mehul garg <mehul.garg@dotpe.in>
2021-03-04 10:54:17 +0530

    Adding set delivery charge UI & UX in the application

--- app/src/main/java/com/digitaldukaan/fragments/SetDeliveryChargeFragment.kt
+++ app/src/main/java/com/digitaldukaan/fragments/SetDeliveryChargeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.digitaldukaan.R
 import com.digitaldukaan.constants.ToolBarManager
 import com.digitaldukaan.services.isInternetConnectionAvailable
+import kotlinx.android.synthetic.main.set_delivery_charge_fragment.*
 
 class SetDeliveryChargeFragment : BaseFragment() {
 
@@ -34,6 +35,78 @@ class SetDeliveryChargeFragment : BaseFragment() {
         if (!isInternetConnectionAvailable(mActivity)) {
             showNoInternetConnectionDialog()
             return
+        }
+        showFreeDeliveryContainer(false)
+        showFixedDeliveryContainer(false)
+        showCustomDeliveryContainer(false)
+    }
+
+    override fun onClick(view: View?) {
+        super.onClick(view)
+        when (view?.id) {
+            freeDeliveryRadioButton.id -> {
+                showFreeDeliveryContainer(true)
+                showFixedDeliveryContainer(false)
+                showCustomDeliveryContainer(false)
+            }
+            fixedDeliveryRadioButton.id -> {
+                showFixedDeliveryContainer(true)
+                showFreeDeliveryContainer(false)
+                showCustomDeliveryContainer(false)
+            }
+            customDeliveryRadioButton.id -> {
+                showCustomDeliveryContainer(true)
+                showFreeDeliveryContainer(false)
+                showFixedDeliveryContainer(false)
+            }
+        }
+    }
+
+    private fun showFreeDeliveryContainer(toShow: Boolean) {
+        if (toShow) {
+            freeDeliveryRadioButton.isChecked = true
+            freeDeliveryRadioButton.isSelected = true
+            freeDeliverySeparator.visibility = View.VISIBLE
+            freeDeliveryTextView.visibility = View.VISIBLE
+        } else {
+            freeDeliveryRadioButton.isChecked = false
+            freeDeliveryRadioButton.isSelected = false
+            freeDeliverySeparator.visibility = View.GONE
+            freeDeliveryTextView.visibility = View.GONE
+        }
+    }
+
+    private fun showFixedDeliveryContainer(toShow: Boolean) {
+        if (toShow) {
+            fixedDeliveryRadioButton.isChecked = true
+            fixedDeliveryRadioButton.isSelected = true
+            fixedDeliverySeparator.visibility = View.VISIBLE
+            fixedDeliveryChargeLayout.visibility = View.VISIBLE
+            freeDeliveryAboveLayout.visibility = View.VISIBLE
+        } else {
+            fixedDeliveryRadioButton.isChecked = false
+            fixedDeliveryRadioButton.isSelected = false
+            fixedDeliverySeparator.visibility = View.GONE
+            fixedDeliveryChargeLayout.visibility = View.GONE
+            freeDeliveryAboveLayout.visibility = View.GONE
+        }
+    }
+
+    private fun showCustomDeliveryContainer(toShow: Boolean) {
+        if (toShow) {
+            customDeliveryRadioButton.isChecked = true
+            customDeliveryRadioButton.isSelected = true
+            customDeliverySeparator.visibility = View.VISIBLE
+            customDeliveryAboveLayout.visibility = View.VISIBLE
+            customDeliveryTextView.visibility = View.VISIBLE
+            customDeliveryImageView.visibility = View.VISIBLE
+        } else {
+            customDeliveryRadioButton.isChecked = false
+            customDeliveryRadioButton.isSelected = false
+            customDeliverySeparator.visibility = View.GONE
+            customDeliveryAboveLayout.visibility = View.GONE
+            customDeliveryTextView.visibility = View.GONE
+            customDeliveryImageView.visibility = View.GONE
         }
     }
 
