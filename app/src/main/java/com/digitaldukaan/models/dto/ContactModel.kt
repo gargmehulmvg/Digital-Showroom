0d0f552fc629a5f3df66027c2d4e1e1cfc1680d5
mehul garg <mehul.garg@dotpe.in>
2021-04-24 19:08:46 +0530

    adding toString method

--- app/src/main/java/com/digitaldukaan/models/dto/ContactModel.kt
+++ app/src/main/java/com/digitaldukaan/models/dto/ContactModel.kt
@@ -5,4 +5,8 @@ import com.google.gson.annotations.SerializedName
 class ContactModel(
     @SerializedName("name") var name: String? = null,
     @SerializedName("number") var number: String? = null
-)
+) {
+    override fun toString(): String {
+        return "ContactModel(name=$name, number=$number)"
+    }
+}
