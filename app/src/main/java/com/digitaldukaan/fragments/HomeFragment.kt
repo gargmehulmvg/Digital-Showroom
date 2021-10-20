1ceeac8bafa41d04daa992adc2368c25ca8126ed
mehul garg <mehul.garg@dotpe.in>
2021-10-20 22:10:05 +0530

    Adding new event in Crashlytics.

--- app/src/main/java/com/digitaldukaan/fragments/HomeFragment.kt
+++ app/src/main/java/com/digitaldukaan/fragments/HomeFragment.kt
@@ -98,6 +98,7 @@ class HomeFragment : BaseFragment(), IHomeServiceInterface,
             setCustomKey("store_id", PrefsManager.getStringDataFromSharedPref(Constants.STORE_ID))
             setCustomKey("store_name", PrefsManager.getStringDataFromSharedPref(Constants.STORE_NAME))
             setCustomKey("mobile_number", PrefsManager.getStringDataFromSharedPref(Constants.USER_MOBILE_NUMBER))
+            setCustomKey("screen_tag", TAG)
         }
     }
 
